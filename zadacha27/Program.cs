afc05720a74678b028a950999be6cd7fb9e8b146
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-11 21:19:50 +0300

    did zadacha27

---
+++ zadacha27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+string str = num.ToString();
+int numLength = str.Length;
+
+for(int i = 0; i < numLength; i++) {
+    int sum1 = int.Parse((str[i].ToString()));
+    sum = sum + sum1;
+}
+
+Console.WriteLine(sum);
