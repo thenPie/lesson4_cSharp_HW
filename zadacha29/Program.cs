a55da1fe73a9c706de60d21f3b34f78d4dd90678
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-12 02:58:41 +0300

    did zadacha29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Массив из восьми рандомных элементов: ");
+
+int Randy() {
+    Random rd = new Random();
+    int rand = rd.Next(1, 9);
+    return rand;
+}
+
+int[] arr = {Randy(), Randy(), Randy(), Randy(), Randy(), Randy(), Randy(), Randy()};
+int lngth = arr.Length;
+
+Console.Write("[");
+for (int i = 0; i < lngth; i ++) {
+    Console.Write(arr[i] + (i < lngth - 1 ? ", " : ""));
+}
+Console.Write("]");
