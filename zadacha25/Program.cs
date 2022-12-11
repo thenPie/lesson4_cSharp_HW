5d37865ea56944849a558ed44dec3e20e6255571
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-11 20:00:06 +0300

    Initial commit, did zadacha25

---
+++ zadacha25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+Console.Write("Введите число, которое необходимо возвести в степень: ");
+int num = int.Parse(Console.ReadLine());
+int original = num;
+Console.Write("Введите степень числа: ");
+int up = int.Parse(Console.ReadLine()) - 1;
+
+for(int i = 0; i < up; i++) {
+    // Console.WriteLine(num);
+    num = num * original;
+    // Console.WriteLine(num);
+}
+
+Console.WriteLine(num);
